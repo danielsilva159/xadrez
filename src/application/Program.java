c3a0b25f036f4a1e211a24fb4fdedf1a20882fcf
projetoUno <provaetb@gmail.com>
2021-03-15 06:55:44 -0300

    Pawn

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessExeption;
@@ -13,19 +15,29 @@ public class Program {
 	
 	 Scanner sc = new Scanner(System.in);
 	 ChessMatch chessMatch = new ChessMatch();
-	 while(true) {
+	 List<ChessPiece>captured = new ArrayList<>();
+	 while(!chessMatch.getCheckMate()) {
 		 try {
 			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces());
+			UI.printMatch(chessMatch, captured);
 			System.out.println();
 			System.out.print("Source: ");
 			ChessPosition source = UI.readChessPosition(sc);
+			
+			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
 			System.out.println();
 			System.out.print("Target: ");
 			ChessPosition target = UI.readChessPosition(sc);
 
 			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+			if(capturedPiece != null) {
+				captured.add(capturedPiece);
+			}
+			
 		 }catch(ChessExeption e) {
 			 System.out.println(e.getMessage());
 			 sc.nextLine();
@@ -34,5 +46,7 @@ public class Program {
 			 sc.nextLine();
 		 }
 	 }
+	 UI.clearScreen();
+	 UI.printMatch(chessMatch, captured);
 }
 }
